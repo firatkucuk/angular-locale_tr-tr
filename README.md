1138cd44020d70770077a6fd40e0fbd851ac115a
Fırat KÜÇÜK <firatkucuk@gmail.com>
2015-07-01 12:02:56 +0300

    utf-8'den kurtarıldı.

---
+++ README.md
@@ -0,0 +1,12 @@
+# AngularJS için yerel paketi
+
+AngularJS'te tüm yerelleri kullanmak için *angular-i18n* paketini tercih ediniz. Eğer uygulamanız yalnızca Türkçe yerelini
+kullanacak ise bu paketi tercih edebilirsiniz.
+
+bower kullanarak kurmak için:
+
+```
+bower install angular-locale_tr-tr
+```
+
+komutunu kullanabilirsiniz. html dosyası içindeki örnek kullanım için test dizinindeki index.html'e gözatabilirsiniz.
--- angular-locale_tr-tr.js
+++ angular-locale_tr-tr.js
@@ -1,119 +1,124 @@
 /*jslint indent: 2, maxerr: 50, maxlen: 120, node: true, nomen: true, plusplus: true, vars: true */
 /*global angular: false */
 
-'use strict';
+// Tek paket halinde yüklemek için:
+// angular-locale_tr-tr paketini tercih edin.
 
-angular.module('ngLocale', [], ['$provide', function ($provide) {
+"use strict";
 
-  $provide.value('$locale', {
-    'id'              : 'tr-tr',
-    'DATETIME_FORMATS': {
-      'AMPMS'         : [
-        'ÖÖ',
-        'ÖS'
+angular.module("ngLocale", [], ["$provide", function ($provide) {
+
+  $provide.value("$locale", {
+    "id"              : "tr-tr",
+    "DATETIME_FORMATS": {
+      "AMPMS"         : [
+        "\u00d6\u00d6",
+        "\u00d6S"
       ],
-      'DAY'           : [
-        'Pazar',
-        'Pazartesi',
-        'Salı',
-        'Çarşamba',
-        'Perşembe',
-        'Cuma',
-        'Cumartesi'
+      "DAY"           : [
+        "Pazar",
+        "Pazartesi",
+        "Sal\u0131",
+        "\u00c7ar\u015famba",
+        "Per\u015fembe",
+        "Cuma",
+        "Cumartesi"
       ],
-      'ERAS'          : [
-        'MÖ',
-        'MS'
+      "ERAS"          : [
+        "M\u00d6",
+        "MS"
       ],
-      'ERANAMES'      : [
-        'Miltattan Önce',
-        'Milattan Sonra'
+      "ERANAMES"      : [
+        "Milattan \u00d6nce",
+        "Milattan Sonra"
       ],
-      'FIRSTDAYOFWEEK': 1,
-      'MONTH'         : [
-        'Ocak',
-        'Şubat',
-        'Mart',
-        'Nisan',
-        'Mayıs',
-        'Haziran',
-        'Temmuz',
-        'Ağustos',
-        'Eylül',
-        'Ekim',
-        'Kasım',
-        'Aralık'
+      "FIRSTDAYOFWEEK": 1,
+      "MONTH"         : [
+        "Ocak",
+        "\u015eubat",
+        "Mart",
+        "Nisan",
+        "May\u0131s",
+        "Haziran",
+        "Temmuz",
+        "A\u011fustos",
+        "Eyl\u00fcl",
+        "Ekim",
+        "Kas\u0131m",
+        "Aral\u0131k"
       ],
-      'SHORTDAY'      : [
-        'Paz',
-        'Pzt',
-        'Sal',
-        'Çar',
-        'Per',
-        'Cum',
-        'Cmt'
+      "SHORTDAY"      : [
+        "Paz",
+        "Pzt",
+        "Sal",
+        "\u00c7ar",
+        "Per",
+        "Cum",
+        "Cmt"
       ],
-      'SHORTMONTH'    : [
-        'Oca',
-        'Şub',
-        'Mar',
-        'Nis',
-        'May',
-        'Haz',
-        'Tem',
-        'Ağu',
-        'Eyl',
-        'Ekm',
-        'Kas',
-        'Ara'
+      "SHORTMONTH"    : [
+        "Oca",
+        "\u015eub",
+        "Mar",
+        "Nis",
+        "May",
+        "Haz",
+        "Tem",
+        "A\u011fu",
+        "Eyl",
+        "Eki",
+        "Kas",
+        "Ara"
       ],
-      'fullDate'      : 'EEEE, dd MMMM y',
-      'longDate'      : 'dd MMMM y',
-      'medium'        : 'dd MMM y hh:mm:ss',
-      'mediumDate'    : 'dd MMM y',
-      'mediumTime'    : 'hh:mm:ss',
-      'short'         : 'dd/MM/yy hh:mm',
-      'shortDate'     : 'dd/MM/yy',
-      'shortTime'     : 'hh:mm'
+      "fullDate"      : "EEEE, dd MMMM y",
+      "longDate"      : "dd MMMM y",
+      "medium"        : "dd MMM y hh:mm:ss",
+      "mediumDate"    : "dd MMM y",
+      "mediumTime"    : "hh:mm:ss",
+      "short"         : "dd/MM/yy hh:mm",
+      "shortDate"     : "dd/MM/yy",
+      "shortTime"     : "hh:mm"
     },
-    'NUMBER_FORMATS'  : {
-      'CURRENCY_SYM': '\u20BA',
-      'DECIMAL_SEP' : ',',
-      'GROUP_SEP'   : '.',
-      'PATTERNS'    : [
+    "NUMBER_FORMATS"  : {
+      "CURRENCY_SYM": "\u20BA",
+      "DECIMAL_SEP" : ",",
+      "GROUP_SEP"   : ".",
+      "PATTERNS"    : [
         { // Ondalık Örüntüsü (Decimal Pattern)
-          'gSize'  : 3,
-          'lgSize' : 3,
-          'maxFrac': 3,
-          'minFrac': 0,
-          'minInt' : 1,
-          'negPre' : '-',
-          'negSuf' : '',
-          'posPre' : '',
-          'posSuf' : ''
+          "gSize"  : 3,
+          "lgSize" : 3,
+          "maxFrac": 3,
+          "minFrac": 0,
+          "minInt" : 1,
+          "negPre" : "-",
+          "negSuf" : "",
+          "posPre" : "",
+          "posSuf" : ""
         },
         { // Para Birimi Örüntüsü (Currency Pattern)
-          'gSize'  : 3,
-          'lgSize' : 3,
-          'maxFrac': 2,
-          'minFrac': 2,
-          'minInt' : 1,
-          'negPre' : '-',
-          'negSuf' : '\u00a0\u00a4',
-          'posPre' : '',
-          'posSuf' : '\u00a0\u00a4'
+          "gSize"  : 3,
+          "lgSize" : 3,
+          "maxFrac": 2,
+          "minFrac": 2,
+          "minInt" : 1,
+          "negPre" : "-",
+          "negSuf" : "\u00a0\u00a4",
+          "posPre" : "",
+          "posSuf" : "\u00a0\u00a4"
         }
       ]
     },
-    'pluralCat'       : function (num) {
+    "pluralCat"       : function (num) {
 
       if (num === 0) {
-        return 'hiç';
-      } else if (num === 1) {
-        return 'bir';
+        return "hiç";
       }
 
-      return 'birçok';
+      if (num === 1) {
+        return "bir";
+      }
+
+      return "birçok";
     }
   });
 }]);
